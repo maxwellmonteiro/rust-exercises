859da1185c89d9e0d6880ffd6e86d36c7732e2c3
maxwell <maxwell.monteiro@gmail.com>
2023-05-22 22:04:11 -0300

    sieve of eratosthenes implemented

--- rust-exercises/prime-numbers/src/main.rs
+++ rust-exercises/prime-numbers/src/main.rs
@@ -2,13 +2,49 @@ mod prime;
 
 use std::ops::RangeInclusive;
 fn main() {
-    let numbers: RangeInclusive<u32> = RangeInclusive::new(1, 100_000_000);
+    let primes: Vec<u32> = get_primes_sieve(100_000_000);
 
-    let result: usize = numbers.into_iter()
-        .filter(|n: &u32| prime::is_prime(n))
-        .map(|n: u32| n.to_string())
-        .count();
+    // print_primes(100);
+    // print_primes_sieve(100);
 
+    println!("primes: {}", primes.iter().count());
+
+}
+
+fn print_primes(end: u32) {
+    let primes: Vec<u32> = get_primes(end);
+
+    let result: String = primes.iter()
+        .map(|v| v.to_string())
+        .collect::<Vec<String>>()
+        .join(", ");
+    
     println!("Primes: {}", result);
+}
+
+fn print_primes_sieve(end: u32) {
+    let primes: Vec<u32> = get_primes_sieve(end);
+
+    let result: String = primes.iter()
+        .map(|v| v.to_string())
+        .collect::<Vec<String>>()
+        .join(", ");
+    
+    println!("Primes: {}", result);
+}
+
+fn get_primes(end: u32) -> Vec<u32> {
+    let numbers: RangeInclusive<u32> = RangeInclusive::new(2, end);
+
+    return numbers
+        .into_iter()
+        .filter(|n: &u32| prime::is_prime(n))
+        .collect();
+}
+
+fn get_primes_sieve(end: u32) -> Vec<u32> {
+    return prime::get_primes_sieve(end)
+        .into_iter()
+        .collect();
 }
 
--- rust-exercises/prime-numbers/src/prime.rs
+++ rust-exercises/prime-numbers/src/prime.rs
@@ -1,3 +1,8 @@
+struct PrimeCheck {
+    value: u32,
+    is_prime: bool
+}
+
 pub fn is_prime(value: &u32) -> bool {
     if *value == 1 {
         return false;
@@ -10,4 +15,32 @@ pub fn is_prime(value: &u32) -> bool {
         }
     }
     return true;
+}
+
+pub fn get_primes_sieve(end: u32) -> Vec<u32> {
+    let mut values: Vec<PrimeCheck> = Vec::with_capacity(((end) + 1) as usize);
+
+    let begin: u32 = 2;
+
+    (begin..=end).for_each(|v| values.push(PrimeCheck{value: v, is_prime: true}));
+
+    let mut begin_value: u32 = (begin as f64).sqrt() as u32;
+    let end_value: u32 = (end as f64).sqrt() as u32;
+
+    begin_value = if begin_value == 1 { 2 } else { begin_value };
+    while begin_value <= end_value {        
+        let mut test_value: u32 = u32::from(begin_value);
+        while test_value < end {
+            test_value += begin_value;
+            let idx = (test_value - begin) as usize;
+            if idx < values.len() {
+                values[idx].is_prime = false;
+            }
+        }
+        begin_value += 1;
+    } 
+    return values.into_iter()
+        .filter(|v| v.is_prime)
+        .map(|v| v.value)
+        .collect();
 }
